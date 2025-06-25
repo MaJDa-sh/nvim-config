9f60e725cf612cd235534fe27fd65afdd97cba39
MaJDa-sh <temakthe02@gmail.com>
2025-06-25 10:32:08 +0200

    undo tree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -241,6 +241,17 @@ local default_plugins = {
       require("which-key").setup(opts)
     end,
   },
+  {
+    "mbbill/undotree",
+    cmd = "UndotreeToggle",
+    keys = {
+      { "<leader>u", "<cmd>UndotreeToggle<CR>", desc = "Toggle Undotree" },
+    },
+    config = function()
+      vim.g.undotree_WindowLayout = 2
+      vim.g.undotree_SplitWidth = 40
+    end,
+  },
 }
 
 local config = require("core.utils").load_config()
